88ca697f31c4501f3ab2cac0a4dc486bd7c6f449
aik <anthonykrueger0@gmail.com>
2021-06-15 20:38:39 -0500

    skeleton done

--- public/JS/server.js
+++ public/JS/server.js
@@ -7,4 +7,29 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const tables = [];
+const tables = [
+    {
+        name: "Sample",
+        email: "Sample@gmail.com",
+        phone: "000-000-0000",
+        unique: 1
+    }
+];
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/../html/home.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, '/../html/reserve.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '/../html/tables.html')));
+
+app.get('/api/tables', (req, res) => res.json(tables));
+
+app.post('/api/tables', (req, res) => {
+    
+    const newTable = req.body;
+    console.log(newTable);
+    tables.push(newTable);
+    res.json(newTable);
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
